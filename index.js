9865333abefc52ad4af40da3a7c7442beccf9807
coolingfan21 <coolingfan21@gmail.com>
2020-02-03 20:50:49 +0900

    initial commit :tada:

---
+++ index.js
@@ -0,0 +1,9 @@
+import { AppRegistry, Platform } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('serendipity_client', () => App);
+
+if (Platform.OS === 'web') {
+  const rootTag = document.getElementById('root') || document.getElementById('main');
+  AppRegistry.runApplication('serendipity_client', { rootTag });
+}
