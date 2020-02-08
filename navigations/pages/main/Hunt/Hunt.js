9b45b889e4728f7096c9f5b06e9c2ad41fd07f99
LEEJAEHYUB <woguqdl20@gmail.com>
2020-02-09 01:04:54 +0900

    SET_HuntPage

--- navigations/pages/main/Hunt/Hunt.js
+++ navigations/pages/main/Hunt/Hunt.js
@@ -2,10 +2,17 @@ import { createStackNavigator } from "react-navigation-stack";
 
 import HuntPage from "./HuntPage";
 
-const HuntPageStack = createStackNavigator({
-  HuntPage: {
-    screen: HuntPage,
+const HuntPageStack = createStackNavigator(
+  {
+    HuntPage: {
+      screen: HuntPage,
+    },
   },
-});
+  {
+    defaultNavigationOptions: {
+      headerShown: false,
+    },
+  },
+);
 
 export default HuntPageStack;
--- navigations/pages/main/Hunt/HuntPage.js
+++ navigations/pages/main/Hunt/HuntPage.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const HuntPage = ({ params }) => (
-  <View>
+const HuntPage = () => (
+  <SafeAreaView>
     <Text>HuntPage</Text>
-  </View>
+  </SafeAreaView>
 );
 
 export default HuntPage;
